d1aef88684e4238313b268182ec408fa71a85fc6
DimKa163 <mitya.martynov.96@gmail.com>
2026-06-16 22:52:34 +0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 )
 
+const (
+	ExitCommand = "exit"
+)
+
 func main() {
 	var line string
 	for {
 		fmt.Print("$ ")
 		fmt.Scanln(&line)
 		switch line {
+		case ExitCommand:
+			return
 		default:
 			fmt.Printf("%s: command not found\n", line)
 		}
