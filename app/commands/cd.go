359578a74388041afc71c8bbbb5b192a07e94e16
DimKa163 <mitya.martynov.96@gmail.com>
2026-06-18 16:22:26 +0400

    codecrafters submit [skip ci]

--- app/commands/cd.go
+++ app/commands/cd.go
@@ -5,11 +5,20 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 const CdCommand = "cd"
 
 func Cd(ctx context.Context, cmd CommandLine) error {
+	arg := cmd.Args()[0]
+	if strings.HasPrefix(arg, "~") {
+		hmDir, err := os.UserHomeDir()
+		if err != nil {
+			return err
+		}
+		arg = strings.Replace(arg, "~", hmDir, 1)
+	}
 	if err := os.Chdir(cmd.Args()[0]); err != nil {
 		var pathErr *os.PathError
 		if errors.As(err, &pathErr) && pathErr.Op == "chdir" {
